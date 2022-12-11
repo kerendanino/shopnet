686cdaca8a711ddc68522084ca3ebf26e2a43d97
Keren Danino <kerendanino@gmail.com>
2022-12-11 22:10:02 +0200

    end of section 3

---
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Core.Entities;
+using Microsoft.Extensions.Logging;
+
+namespace Infrastructure.Data
+{
+    public class StoreContextSeed
+    {
+        public static async Task SeedAsync(StoreContext context, ILoggerFactory loggerFactory)
+        {
+            try
+            {
+                if (!context.ProductBrands.Any())
+                {
+                    var brandData = File.ReadAllText("../Infrastructure/Data/SeedData/brands.json");
+
+                    var brand = JsonSerializer.Deserialize<List<ProductBrand>>(brandData);
+
+                    foreach (var item in brand)
+                    {
+                        context.ProductBrands.Add(item);
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+
+                if (!context.ProductTypes.Any())
+                {
+                    var typeData = File.ReadAllText("../Infrastructure/Data/SeedData/types.json");
+
+                    var type = JsonSerializer.Deserialize<List<ProductType>>(typeData);
+
+                    foreach (var item in type)
+                    {
+                        context.ProductTypes.Add(item);
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+
+                if (!context.Products.Any())
+                {
+                    var productsData = File.ReadAllText("../Infrastructure/Data/SeedData/products.json");
+
+                    var products = JsonSerializer.Deserialize<List<Product>>(productsData);
+
+                    foreach (var item in products)
+                    {
+                        context.Products.Add(item);
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+            }
+            catch (System.Exception ex)
+            {
+                var logger = loggerFactory.CreateLogger<StoreContextSeed>();
+                logger.LogError(ex.Message);
+            }
+        }
+    }
+}
